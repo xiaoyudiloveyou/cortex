6f7b67db028d5cf46175c4f7b3381a84aab85440
Bryan Boreham <bryan@weave.works>
2019-03-12 17:58:49 +0000

    Use image tag from current build
    Signed-off-by: Bryan Boreham <bryan@weave.works>

--- integration-tests/common.sh
+++ integration-tests/common.sh
@@ -1,5 +1,9 @@
 # file intended to be sourced by other shell scripts
 
+TEST_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+IMAGE_TAG=$($TEST_DIR/../tools/image-tag)
+
 COMMON_ARGS="-consul.hostname=consul:8500"
 STORAGE_ARGS="-chunk.storage-client=aws -dynamodb.chunk-table.from=2019-01-01 -dynamodb.url=dynamodb://dynamodb -s3.url=placeholder"
 INGESTER_ARGS="$COMMON_ARGS $STORAGE_ARGS -ingester.num-tokens=4 -ingester.min-ready-duration=1s -ingester.claim-on-rollout=true"
--- integration-tests/start-components.sh
+++ integration-tests/start-components.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-. "$(dirname "$0")/common.sh"
+THIS_DIR=$(dirname "$0")
+. "$THIS_DIR/common.sh"
 
 docker network create cortex
 
 docker run $RUN_ARGS -d --name=consul --hostname=consul consul:0.7.1 agent -ui -server -client=0.0.0.0 -bootstrap
 docker run $RUN_ARGS -d --name=dynamodb --hostname=dynamodb deangiberson/aws-dynamodb-local
-docker run $RUN_ARGS -d --name=distributor --hostname=distributor -p 8080:80 quay.io/cortexproject/distributor $COMMON_ARGS -distributor.replication-factor=1
+docker run $RUN_ARGS -d --name=distributor --hostname=distributor -p 8080:80 quay.io/cortexproject/distributor:$IMAGE_TAG $COMMON_ARGS -distributor.replication-factor=1
