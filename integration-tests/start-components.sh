762cbf204363d066e1ec99900c5981b1e9031b2e
Bryan Boreham <bryan@weave.works>
2019-04-25 09:15:40 +0000

    Use a newer Consul version in tests
    Signed-off-by: Bryan Boreham <bryan@weave.works>

--- integration-tests/start-components.sh
+++ integration-tests/start-components.sh
@@ -7,6 +7,6 @@ THIS_DIR=$(dirname "$0")
 
 docker network create cortex
 
-docker run $RUN_ARGS -d --name=consul --hostname=consul consul:0.7.1 agent -ui -server -client=0.0.0.0 -bootstrap
+docker run $RUN_ARGS -d --name=consul --hostname=consul consul:0.9 agent -ui -server -client=0.0.0.0 -bootstrap
 docker run $RUN_ARGS -d --name=dynamodb --hostname=dynamodb amazon/dynamodb-local -jar DynamoDBLocal.jar -inMemory -sharedDb
 docker run $RUN_ARGS -d --name=distributor --hostname=distributor -p 8080:80 quay.io/cortexproject/distributor:$IMAGE_TAG $COMMON_ARGS -distributor.replication-factor=1
